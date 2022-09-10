7eea7182e1e66e6710cef74b91bf1e9d01ce5c21
adrianVelasco <64821523+Luadveand@users.noreply.github.com>
2022-09-10 17:33:09 -0500

    Initial commit

---
+++ Mecanillama.API/Security/Resources/UserResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Mecanillama.API.Security.Resources;
+
+public class UserResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+}
