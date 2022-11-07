d1d791d420584ceee5837a11436bd9240fdd45d1
adrianVelasco <64821523+Luadveand@users.noreply.github.com>
2022-11-07 11:27:23 -0500

    feat: Added new integrations tests

--- Mecanillama.ITests/IntegrationTests.cs
+++ Mecanillama.ITests/IntegrationTests.cs
@@ -7,7 +7,7 @@ namespace Mecanillama.ITests
     public class IntegrationTests
     {
         [TestMethod]
-        public async Task DefaultRoute_Login()
+        public async Task Customers_Get()
         {
             var webAppFactory = new WebApplicationFactory<Program>();
             var httpClient = webAppFactory.CreateDefaultClient();
@@ -16,6 +16,40 @@ namespace Mecanillama.ITests
             var stringResult = await response.Content.ReadAsStringAsync();
             var search = stringResult.Contains("customer");
             Assert.IsTrue(search);
+        }
+        [TestMethod]
+        public async Task Appointments_Get()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var response = await httpClient.GetAsync("/api/v1/Appointments");
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var search = stringResult.Contains("10:00");
+            Assert.IsTrue(search);
+        }
+        
+        [TestMethod]
+        public async Task Mechanics_Get()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var response = await httpClient.GetAsync("/api/v1/Mechanics");
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var search = stringResult.Contains("mechanic");
+            Assert.IsTrue(search);
+        }
+        
+        [TestMethod]
+        public async Task Reviews_Get()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var response = await httpClient.GetAsync("/api/v1/Reviews");
+            var stringResult = await response.Content.ReadAsStringAsync();
+            Assert.IsNotNull(stringResult);
         }
     }
 }
